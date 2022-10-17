8c04c5f933bdbb57c2c78355f106cba14046bbb8
Onyi Nkisi <oc.nkisiorji@gmail.com>
2022-10-17 08:10:05 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,11 @@
 #include <stdlib.h>
 
 /**
- *
  *  main - Entry point
- *
  *  Return: Always 1 (Success)
- *
  */
-
 int main(void)
-
 {
-		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-		return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
